c738d571756c06053689d399bef3c50a7643d33c
aarsh0 <145794864+aarsh0@users.noreply.github.com>
2024-01-19 23:27:35 +0530

    Change in images
    Few changes in links for images

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -17,7 +17,7 @@ const FeaturedProperties = () => {
             </div>
 
             <div className="featuredPropertyItem">
-                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="propertyImg" />
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/87428762.webp?k=de5db8fe94cbfe08d3bf16d3c86def035fd73b43ee497cffe27b03363764e0e2&o" alt="propertyImg" />
                 <div className="propertyDesc">
                     <p className="propertyName">7Seasons Apartments</p>
                     <p className="propertyLocation">Hungary</p>
@@ -30,7 +30,7 @@ const FeaturedProperties = () => {
             </div>
 
             <div className="featuredPropertyItem">
-                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/477071028.webp?k=c9d87661dfac89970e50379629aa316a4905033f8f4a13cb21fd28484256d626&o=" alt="propertyImg" />
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/85257658.webp?k=e3f110e4ed0978310a028465a3bdd609149ecbded601555c881106255556b52e&o=" alt="propertyImg" />
                 <div className="propertyDesc">
                     <p className="propertyName">Oriente Palace</p>
                     <p className="propertyLocation">Spain</p>

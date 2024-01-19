31c8a2302a58a8025eb6f6de251daa1db97ebb40
aarsh0 <145794864+aarsh0@users.noreply.github.com>
2024-01-19 23:22:45 +0530

    Change in link of a image
    Changed a link for an image that was not showing due to a bug

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -17,7 +17,7 @@ const FeaturedProperties = () => {
             </div>
 
             <div className="featuredPropertyItem">
-                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/87428762.webp?k=de5db8fe94cbfe08d3bf16d3c86def035fd73b43ee497cffe27b03363764e0e2&o=" alt="propertyImg" />
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="propertyImg" />
                 <div className="propertyDesc">
                     <p className="propertyName">7Seasons Apartments</p>
                     <p className="propertyLocation">Hungary</p>
